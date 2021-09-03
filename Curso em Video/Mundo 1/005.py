27145ba1f98c317979cd7a61f7e5a07edd84aab7
pqsnow <joamorcatti@gmail.com>
2021-09-02 21:31:11 -0300

    Create 005.py

---
+++ Curso em Video/Mundo 1/005.py
@@ -0,0 +1,5 @@
+numero = int(input('Número: ')) 
+antecessor = numero - 1
+sucessor = numero + 1
+
+print("O sucessor de {} é {} e o antecessor é {}".format(numero, sucessor, antecessor)) #outra opção é remover as duas ultimas variaveis e add .format(numero, (numero - 1), (numero + 2))
