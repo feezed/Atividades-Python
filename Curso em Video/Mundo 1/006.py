2aaa2d6b5555b08f8e63eafae729a05bdf10df4c
pqsnow <joamorcatti@gmail.com>
2021-09-02 21:31:13 -0300

    Create 006.py

---
+++ Curso em Video/Mundo 1/006.py
@@ -0,0 +1,6 @@
+numero = int(input('Número: '))
+dobro = numero * 2
+triplo = numero * 3
+raiz = numero ** (1/2)
+
+print('O número é: {}, o dobro é: {}, o triplo é: {}, a raiz é: {:.2f}.'.format(numero,dobro,triplo,raiz))
