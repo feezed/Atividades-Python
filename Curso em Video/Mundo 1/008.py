ae346a19c52d504f33a71845ab8eba28a067481f
pqsnow <joamorcatti@gmail.com>
2021-09-02 21:31:17 -0300

    Create 008.py

---
+++ Curso em Video/Mundo 1/008.py
@@ -0,0 +1,5 @@
+metros = float(input("Digite os metros: "))
+cent = metros * 100
+mili = metros * 1000
+
+print('{} metros convertidos em centímetros = {:.0f}. \nE convertido para milímetros = {:.0f}'.format(metros, cent, mili))
